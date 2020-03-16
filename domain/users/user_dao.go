5cca157c148565bce5869f1abfa8a9d93c38be67
willqiang <willqiang32@gmail.com>
2020-03-16 16:46:53 +0800

    configure MySQL

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"fmt"
+	"github.com/willqiang/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/willqiang/bookstore_users-api/utils/date_utils"
 	"github.com/willqiang/bookstore_users-api/utils/errors"
 )
@@ -11,6 +12,9 @@ var (
 )
 
 func (user *User) Get() *errors.RestErr {
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
 	result := userDB[user.Id]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
